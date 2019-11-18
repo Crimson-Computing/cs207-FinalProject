1ad61ca15c034e8bb5d387212cc93dfc12f7656b
xuzhiying9510 <xuzhiying9510@xuzhiying9510@hotmail.com>
2019-11-17 19:39:45 -0500

    change for codecov

--- tests/test_autodiffcc.py
+++ tests/test_autodiffcc.py
@@ -66,6 +66,49 @@ def test_rsub():
     assert t1.val == 1
     assert t1.der == pytest.approx(-0.3333333333333333)
 
+def test_pow():
+    t1 = AD(val = 3, der = 1) ** 2
+    assert t1.val == 9
+    assert t1.der == 6
+    t2 = AD(val = 3, der = 1) ** AD(val = 5, der = 1)
+    assert t2.val == 243
+    assert t2.der == pytest.approx(671.96278615)
+
+def test_rpow():
+    t1 = 2 ** AD(val = 3, der = 1)
+    assert t1.val == 8
+    assert t1.der == pytest.approx(5.54517744)
+
+def test_eq():
+    t1 = AD(val = 3, der = 1)
+    assert t1 == 3
+    t2 = AD(val = 3, der = 1)
+    assert t1 == t2
+
+def test_gt():
+    t1 = AD(val = 3, der = 1)
+    assert t1 > 2
+    t2 = AD(val = 2, der = 1)
+    assert t1 > t2
+
+def test_ge():
+    t1 = AD(val = 3, der = 1)
+    assert t1 >= 2
+    t2 = AD(val = 3, der = 1)
+    assert t1 >= t2
+
+def test_lt():
+    t1 = AD(val = 3, der = 1)
+    assert t1 < 4
+    t2 = AD(val = 4, der = 1)
+    assert t1 < t2
+
+def test_le():
+    t1 = AD(val = 3, der = 1)
+    assert t1 <= 3
+    t2 = AD(val = 3, der = 1)
+    assert t1 <= t2
+
 def test_combination():
     t1 = (AD(val = 3, der = 1) / 3  + 1) * 6 - 4
     assert t1.val == 8
