04298bb2eb12ea31ea885a555bef5f688746394b
Matthew Finney <MattFinney@users.noreply.github.com>
2019-11-19 13:05:04 -0500

    Update tests/test_autodiffcc.py
    Fixing the missing colon so that Travis will work

--- tests/test_autodiffcc.py
+++ tests/test_autodiffcc.py
@@ -7,7 +7,7 @@ from autodiffcc import AD
 def test_wrong_shape():
     try:
         t1 = AD(val = np.array([2,3,1]), der = np.array([-1,3]))
-    except Exception
+    except: Exception
 
 def test_pos():
     t1 = +AD(val = 3, der = 1)
