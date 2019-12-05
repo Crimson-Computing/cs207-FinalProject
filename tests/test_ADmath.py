4ed5b3a43bc5d80f987b508b167ade1e17d41e75
mattfinney <MattFinney@users.noreply.github.com>
2019-12-05 09:01:26 -0500

    Fixing a mistake I made requiring ValueError when TypeError was the exception actually raised by the ADmath functions.

--- tests/test_ADmath.py
+++ tests/test_ADmath.py
@@ -7,7 +7,7 @@ def test_cos():
     t1 = ADmath.cos(AD(val=3, der=1))
     assert t1.val == pytest.approx(-0.9899924966004454)
     assert t1.der == pytest.approx(-0.1411200080598672)
-    with pytest.raises(ValueError, match=r".* This function can only take AD"
+    with pytest.raises(TypeError, match=r".* This function can only take AD"
                                          r" objects as inputs .*"):
         t2 = ADmath.cos(1)
 
@@ -16,7 +16,7 @@ def test_sin():
     t1 = ADmath.sin(AD(val=3, der=1))
     assert t1.val == pytest.approx(0.1411200080598672, 0.1)
     assert t1.der == pytest.approx(-0.9899924966004454)
-    with pytest.raises(ValueError, match=r".* This function can only take AD"
+    with pytest.raises(TypeError, match=r".* This function can only take AD"
                                          r" objects as inputs .*"):
         t2 = ADmath.sin(1)
 
@@ -25,7 +25,7 @@ def test_tan():
     t1 = ADmath.tan(AD(val=3, der=1))
     assert t1.val == pytest.approx(-0.1425465430742778)
     assert t1.der == pytest.approx(1.020319516942427)
-    with pytest.raises(ValueError, match=r".* This function can only take AD"
+    with pytest.raises(TypeError, match=r".* This function can only take AD"
                                          r" objects as inputs .*"):
         t2 = ADmath.tan(1)
 
@@ -34,7 +34,7 @@ def test_exp():
     t1 = ADmath.exp(AD(val=3, der=1))
     assert t1.val == pytest.approx(20.085536923187668)
     assert t1.der == pytest.approx(20.085536923187668)
-    with pytest.raises(ValueError, match=r".* This function can only take AD"
+    with pytest.raises(TypeError, match=r".* This function can only take AD"
                                          r" objects as inputs .*"):
         t2 = ADmath.exp(1)
 
@@ -43,6 +43,6 @@ def test_sqrt():
     t1 = ADmath.sqrt(AD(val=3, der=1))
     assert t1.val == pytest.approx(1.7320508075688772)
     assert t1.der == pytest.approx(0.28867513459481287)
-    with pytest.raises(ValueError, match=r".* This function can only take AD"
+    with pytest.raises(TypeError, match=r".* This function can only take AD"
                                          r" objects as inputs .*"):
         t2 = ADmath.sqrt(1)
