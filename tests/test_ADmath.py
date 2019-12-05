e780bbe0df89e76ea02accc7b03265e9541a2600
mattfinney <MattFinney@users.noreply.github.com>
2019-12-05 09:53:40 -0500

    Updates to test_ADmath.py
    Adjusting TypeError matching to be generalizeable to all TypeErrors resulting from not executing the Admath functions on an AD object (e.g., not specific TypeError text for sin that doesn't work with cos or exp.)

--- tests/test_ADmath.py
+++ tests/test_ADmath.py
@@ -15,8 +15,7 @@ def test_sin():
     t1 = ADmath.sin(AD(val=3, der=1))
     assert t1.val == pytest.approx(0.1411200080598672, 0.1)
     assert t1.der == pytest.approx(-0.9899924966004454)
-    with pytest.raises(TypeError, match=r".* This function can only take AD"
-                                        r" objects as inputs .*"):
+    with pytest.raises(TypeError,  match=r"This function can only take AD objects as inputs"):
         t2 = ADmath.sin(1)
 
 
@@ -24,8 +23,7 @@ def test_tan():
     t1 = ADmath.tan(AD(val=3, der=1))
     assert t1.val == pytest.approx(-0.1425465430742778)
     assert t1.der == pytest.approx(1.020319516942427)
-    with pytest.raises(TypeError, match=r".* This function can only take AD"
-                                        r" objects as inputs .*"):
+    with pytest.raises(TypeError,  match=r"This function can only take AD objects as inputs"):
         t2 = ADmath.tan(1)
 
 
@@ -33,8 +31,7 @@ def test_exp():
     t1 = ADmath.exp(AD(val=3, der=1))
     assert t1.val == pytest.approx(20.085536923187668)
     assert t1.der == pytest.approx(20.085536923187668)
-    with pytest.raises(TypeError, match=r".* This function can only take AD"
-                                        r" objects as inputs .*"):
+    with pytest.raises(TypeError,  match=r"This function can only take AD objects as inputs"):
         t2 = ADmath.exp(1)
 
 
@@ -42,6 +39,5 @@ def test_sqrt():
     t1 = ADmath.sqrt(AD(val=3, der=1))
     assert t1.val == pytest.approx(1.7320508075688772)
     assert t1.der == pytest.approx(0.28867513459481287)
-    with pytest.raises(TypeError, match=r".* This function can only take AD"
-                                        r" objects as inputs .*"):
+    with pytest.raises(TypeError,  match=r"This function can only take AD objects as inputs"):
         t2 = ADmath.sqrt(1)
