fff88b2608f6df2723106a9b5d494c98a6b98fbd
mattfinney <MattFinney@users.noreply.github.com>
2019-12-09 00:26:18 -0500

    Consolidated root functions in one module, updated docstrings, standardized some of the functions, add max_iter to _bisect method, added matplotlib to requirements for pyplot dependency in bisect.

--- autodiffcc/root.py
+++ autodiffcc/root.py
@@ -1,6 +1,107 @@
 import inspect
+import matplotlib.pyplot as plt
 import numpy as np
-from autodiffcc.core import AD, differentiate
+import itertools
+from autodiffcc.core import differentiate
+
+
+def _check_start_values(start_values, signature):
+    """ A service that checks the shape of start_values and reformats them for the specific method
+    INPUTS
+    =======
+    start_values: a list or dictionary of start values provided to the root finder
+    signature: a signature of a function provided to the root finder
+
+    RETURNS
+    ========
+    returns an array of values to pass to the specific root finding method
+    """
+    if not start_values:
+        raise ValueError("Must include start_values as dict or list/array for this method.")
+
+    if isinstance(start_values, dict):
+        # turn keyword values into positional values matching function signature
+        values = []
+        for key in signature:
+            try:
+                values.append(start_values[key])
+            except KeyError:
+                raise KeyError(f"key {key} in function signature missing from start_values")
+        if len(start_values.keys()) != len(signature):
+            raise KeyError("Too many keys passed in start_values dictionary")
+    elif isinstance(start_values, (list, np.ndarray)):
+        # if list-like
+        values = start_values
+    elif np.isscalar(start_values):
+        values = [start_values]
+    else:
+        raise TypeError("Must include start_values as dict or list/array.")
+
+    values = np.array(values)
+
+    # check to make sure have correct number of variables
+    if len(values) != len(signature):
+        raise KeyError("Incorrect number of variables passed in start_values.")
+
+    return values
+
+
+def _check_interval(interval, signature):
+    """ A service that checks the shape of interval and reformats it for the specific method
+
+    INPUTS
+    =======
+    intevral: a list/array or a list of two dictionaries [interval_start, interval_end] provided to the root finder
+    signature: a signature of a function provided to the root finder
+
+    RETURNS
+    ========
+    returns two arrays interval_start, interval_end to pass to the specific root finding method
+    """
+
+    if interval is None:
+        raise ValueError("Must provide interval for this method.")
+
+    if isinstance(interval[0], dict) and isinstance(interval[1], dict):
+        # turn keyword values into positional values matching function signature
+        interval_start_values = []
+        start_dict = interval[0]
+
+        interval_end_values = []
+        end_dict = interval[1]
+
+        if len(start_dict.keys()) != len(end_dict.keys()):
+            raise KeyError("The interval_start and interval_end dictionaries must have the same number of keys")
+
+        for key in signature:
+            try:
+                interval_start_values.append(start_dict[key])
+                interval_end_values.append(end_dict[key])
+            except KeyError:
+                raise KeyError(f"key {key} in function signature missing from interval dictionary")
+        if len(start_dict.keys()) != len(signature):
+            raise KeyError("Too many keys passed in interval dictionary")
+
+        interval_start = np.asarray(interval_start_values)
+        interval_end = np.asarray(interval_end_values)
+
+    elif isinstance(interval, (list, np.ndarray)):
+        # if list-like
+        values = np.asarray(interval)
+        interval_start = values[:, 0]
+        interval_end = values[:, 1]
+
+    else:
+        raise TypeError("Must include interval as list of two dicts or list/array.")
+
+    # check to make sure have correct number of variables
+    if len(interval_start) != len(signature):
+        raise KeyError("Incorrect number of variables passed in interval.")
+
+    # interval_start = interval_start.reshape(-1,1)
+    # interval_end = interval_end.reshape(-1,1)
+
+    return interval_start, interval_end
 
 
 def _norm(vector):
@@ -23,8 +124,171 @@ def _norm(vector):
     return np.sum(np.abs(vector))
 
 
+def _bisect(function, interval_start, interval_end, max_iter, signature):
+    """Performs the bisection method on a function of one or more variables to find a root
+
+    INPUTS
+    =======
+    function: a function using ADmath methods
+    start_values: the starting point of the root-finding method (scalar or vector)
+    interval_start: an array of the start of the interval for all variables
+    interval_end: an array of the end of the interval for all variables
+    max_iter: maximum number of iterations that algorithm will look for before quitting
+
+    RETURNS
+    ========
+    returns the approximate root
+
+    NOTES
+    =====
+    This function considers a value to be 0 if round(value, 15) == 0)
+    This function does not find all the roots. You can change the intervals to
+    look for different roots.
+
+    EXAMPLES
+    =========
+
+    >>> def f(x, y):
+    >>>    return(2*x*y - 2)
+
+    >>> _bisect(f, interval_start = [-10, 10], interval_end = [-4, 10])
+    [0.12999772724065328, 7.692442177460448]
+    """
+
+    print("Please note that this function uses approximations")
+
+    # check how many parameters there are in the function
+    nParam = len(signature)
+
+    # create a point array storing coordinates
+
+    ## example of points
+    #    start-interval finish-interval
+    # x       1               2
+    # y       3               10
+    # z      -2               0
+
+    points = np.c_[interval_start, interval_end]
+
+    #### PLOT if can do 2D graph
+    if nParam == 1:
+        # x axis values
+        a = interval_start
+        b = interval_end
+        interval = np.linspace(a, b, num=1000)
+        # corresponding y axis values
+        values = function(interval)
+        # plotting the points
+        plt.plot(interval, values, color='green', linestyle='dashed', linewidth=0.5,
+                 marker='o', markerfacecolor='blue', markersize=1)
+        # naming the x axis
+        plt.xlabel(' Interval ')
+        # naming the y axis
+        plt.ylabel(' Values ')
+        plt.axhline(y=0)
+        # giving a title to my graph
+        plt.title('Graphs function in the specified interval')
+        # function to show the plot
+        plt.show()
+
+        # get the starting points of each variable
+    # get the ending points for each variable
+    # get their combinations. Should be 4 different combinations
+
+    matrix = np.empty((nParam, 2))
+    for p in range(0, nParam):
+        matrix[p] = points[p]
+    allpoints = list(itertools.product(*matrix))
+
+    # check sign change
+    results = []
+    for elements in allpoints:
+        results.append(function(*elements))
+        asign = np.sign(results)
+        # detect sign change
+        signchange = sum(((np.roll(asign, 1) - asign) != 0).astype(int)) > 0
+
+    # if signs are not different
+    if not signchange:
+        raise Exception("No change in sign, please try different intervals")
+
+    if signchange:
+        print("Root between in the specified intervals")
+        i = 1
+        # if signs are different
+        while signchange:
+            print("----------------------")
+            print("iteration", i)
+
+            i = i + 1
+
+            # middle points
+            c = []
+            for k in range(0, nParam):
+                c.append((points[k][0] + points[k][1]) / 2)
+
+            middlePointResult = function(*c)
+
+            # approx to 14ths decimal point
+            # if found root:
+            if (round(middlePointResult, 15) == 0):
+                print("root found for ", c)
+                return (c)  # return middle as the approximate root value
+            # if did not find root yet:
+            else:
+                j = 0
+
+                for n in results:
+                    if (n * middlePointResult < 0):
+                        print("root between", c, "and", allpoints[j])
+                        corner1 = list(allpoints[j])
+                        corner2 = c
+                    j = j + 1
+                print("choosing to look in the area between", corner1, "and", corner2)
+
+                # update points for intervals
+                if corner1 > corner2:
+                    points = np.c_[corner2, corner1]
+                else:
+                    points = np.c_[corner1, corner2]
+
+                points = np.asarray(points)
+
+                matrix = np.empty((nParam, 2))
+                for p in range(0, nParam):
+                    matrix[p] = points[p]
+                allpoints = list(itertools.product(*matrix))
+
+                # check sign change
+                results = []
+                for elements in allpoints:
+                    results.append(function(*elements))
+                    asign = np.sign(results)
+                    # detect sign change
+                    signchange = sum(((np.roll(asign, 1) - asign) != 0).astype(int)) > 0
+
+            if i >= max_iter:
+                break
+
+
 def _newton_raphson(function, values, threshold, max_iter):
-    """#TODO
+    """Returns the root of the function found using the specified method
+
+    INPUTS
+    =======
+    function: a function using ADmath methods
+    start_values: the starting point of the root-finding method (scalar or vector)
+    threshold: the minimum threshold for finding a root
+    max_iter: maximum number of iterations that algorithm will look for before quitting
+        - in case method does not converge
+
+    RETURNS
+    ========
+    the root of the function found using the specified method
+
+    EXAMPLES
+    =========
+    # TODO
     """
     jacobian = differentiate(function)
 
@@ -40,22 +304,74 @@ def _newton_raphson(function, values, threshold, max_iter):
     raise Exception("Newton-Raphson did not converge, try increasing max_iter.")
 
 
-def _method_2(function, start, threshold):
-    pass
-
-
-def _method_3(function, start, threshold):
-    pass
-
-
-def find_root(function, method, start_values, threshold=1e-8, max_iter=2000):
+def _newton_fourier(function, interval_start: np.ndarray, interval_end: np.ndarray, threshold, max_iter):
     """Returns the root of the function found using the specified method
 
     INPUTS
     =======
     function: a function using ADmath methods
-    start: the starting point of the root-finding method (scalar or vector)
-    method: the method to do root-finding   #TODO: add list of possible methods
+    interval_start: an array of the start of the interval for all variables
+    interval_end: an array of the end of the interval for all variables
+    threshold: the minimum threshold for finding a root
+    max_iter: maximum number of iterations that algorithm will look for before quitting
+        - in case method does not converge
+
+    RETURNS
+    ========
+    a root of the function found using the specified method on the interval within the threshold
+
+    EXAMPLES
+    =========
+    >>> def f(x, y):
+    >>>     return 2 * x + y - 2, y+2
+    >>> interval_start = [3, -3]
+    >>> interval_end = [3, -3]
+    >>> my_root = _newton_fourier(f, interval_start=interval, interval_end=interval_end, threshold=1e-8, max_iter=2000)
+    >>> print(my_root)
+    [ 2. -2.]    """
+    # using positional variables
+    x_vars = interval_start
+    z_vars = interval_end
+
+    # Starting values for x_0, z_0
+    flat_x = x_vars.flatten()
+    flat_z = z_vars.flatten()
+    limit_numerator = (x_vars.flatten() - z_vars.flatten())**2
+
+    jacobian = differentiate(function)
+
+    for i in range(max_iter):
+        if len(flat_x) == 1:
+            common_jacobian = jacobian(*x_vars)
+            flat_x = flat_x - function(*x_vars) / common_jacobian
+            flat_z = flat_z - function(*z_vars) / common_jacobian
+        else:
+            common_jacobian = np.linalg.pinv(jacobian(*x_vars))
+            flat_x = flat_x - np.matmul(common_jacobian, function(*x_vars))
+            flat_z = flat_z - np.matmul(common_jacobian, function(*z_vars))
+
+        limit_denominator = limit_numerator
+        limit_numerator = flat_x - flat_z
+        limit = limit_numerator/limit_denominator
+
+        x_vars = flat_x.reshape(x_vars.shape)
+        z_vars = flat_z.reshape(z_vars.shape)
+
+        if limit.all() < threshold:
+            return np.mean(np.vstack([x_vars, z_vars]), axis=0)
+
+    raise Exception("Newton-Fourier did not converge, try increasing max_iter.")
+
+
+def find_root(function, start_values=None, interval=None, method='newton-raphson', threshold=1e-8, max_iter=2000):
+    """Returns the root of the function found using the specified method
+
+    INPUTS
+    =======
+    function: a function using ADmath methods
+    start_values: the starting point of the root-finding method (scalar or vector)
+    interval: the starting interval of the root-finding method, either as an array or a list of two dicts
+    method: the method to do root-finding ['newton-raphson', 'newton-fourier', 'bisect']
     threshold: the minimum threshold for finding a root
     max_iter: maximum number of iterations that algorithm will look for before quitting
         - in case method does not converge
@@ -66,35 +382,25 @@ def find_root(function, method, start_values, threshold=1e-8, max_iter=2000):
 
     EXAMPLES
     =========
-    >>> x = AD(val = 3, der = 1)
-    >>> 2 ** x
-    (8.0, 5.54517744)
+    >>> def f(x, y):
+    >>>     return 2 * x + y - 2, y+2
+    >>> interval = [[3, -3], [3, -3]]
+    >>> my_root = find_root(f, interval=interval, method='newton-fourier', threshold=1e-8, max_iter=2000)
+    >>> print(my_root)
+    [ 2. -2.]
     """
     # process variable inputs
     signature = inspect.signature(function).parameters.keys()
 
-    if isinstance(start_values, dict):
-        # turn keyword values into positional values matching function signature
-        values = []
-        for key in signature:
-            try:
-                values.append(start_values[key])
-            except KeyError:
-                raise KeyError(f"key {key} in function signature missing from start_values")
-        if len(start_values.keys()) != len(signature):
-            raise KeyError("Too many keys passed in start_values dictionary")
-    elif isinstance(start_values, (list, np.ndarray)):
-        # if list-like
-        values = start_values
-    elif np.isscalar(start_values):
-        values = [start_values]
-    else:
-        raise TypeError("Must include start_values as dict or list/array.")
-    values = np.array(values)
-    # check to make sure have correct number of variables
-    if len(values) != len(signature):
-        raise KeyError("Incorrect number of variables passed in start_values.")
-
     # find roots
     if method.lower() in ['newton', 'newton-raphson', 'n-r']:
+        values = _check_start_values(start_values=start_values, signature=signature)
         return _newton_raphson(function, values, threshold, max_iter)
+
+    if method.lower() in ['bisect', 'bisection', 'b']:
+        interval_start, interval_end = _check_interval(interval=interval, signature=signature)
+        return _bisect(function, interval_start, interval_end, max_iter, signature)
+
+    if method.lower() in ['newton-fourier', 'n-f']:
+        interval_start, interval_end = _check_interval(interval=interval, signature=signature)
+        return _newton_fourier(function, interval_start, interval_end, threshold, max_iter)
